24aea9222ca055a782128d8ec97c59e8999f2db8
JiHwan Shin <jumprope123@naver.com>
2020-09-26 15:21:41 +0900

    TedPermission을 활용해서 CALL_PHONE 권한 승인 처리

--- app/src/main/java/kr/co/tjoeun/library_20200926/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200926/MainActivity.kt
@@ -1,10 +1,14 @@
 package kr.co.tjoeun.library_20200926
 
+import android.Manifest
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -16,13 +20,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-    }
-
-    override fun setValues() {
 
         loadWebImageBtn.setOnClickListener {
 
-        Glide.with(mContext).load("https://search.pstatic.net/common/?src=http%3A%2F%2Fpost.phinf.naver.net%2FMjAxOTAxMDJfMjYx%2FMDAxNTQ2NDE1MTIxMDMz.Rjbz_7nebW4wpWAjhWvXzDDyNMB4C31s2jMUdDM5enEg.eVAS4Sr02RslUK46NcB8KORsBTROm87BAvZtihYQFJQg.JPEG%2FIoNeRDLzprO3EPNqH0JwGq2sa6UU.jpg&type=sc960_832").into(otherPersonImg)
+            Glide.with(mContext).load("https://search.pstatic.net/common/?src=http%3A%2F%2Fpost.phinf.naver.net%2FMjAxOTAxMDJfMjYx%2FMDAxNTQ2NDE1MTIxMDMz.Rjbz_7nebW4wpWAjhWvXzDDyNMB4C31s2jMUdDM5enEg.eVAS4Sr02RslUK46NcB8KORsBTROm87BAvZtihYQFJQg.JPEG%2FIoNeRDLzprO3EPNqH0JwGq2sa6UU.jpg&type=sc960_832").into(otherPersonImg)
 
         }
 
@@ -33,5 +34,36 @@ class MainActivity : BaseActivity() {
             startActivity(myIntent)
 
         }
+
+        callPhoneBtn.setOnClickListener {
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    
+                    val phoneNum = phoneNumTxt.text.toString()
+
+                    val myUri = Uri.parse("tel:${phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL,myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "내용이 거부되어 통화불가", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        TedPermission.with(mContext)
+            .setPermissionListener(pl)
+            .setDeniedMessage("권한이 거부되었습니다.")
+            .setPermissions(Manifest.permission.CALL_PHONE)
+            .check()
+
+        }
+
+    }
+
+    override fun setValues() {
+
+
     }
 }
